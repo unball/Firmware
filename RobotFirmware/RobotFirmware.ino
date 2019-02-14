481a7bda101abda32e1ff8233076d2ac63609c6e
makueba <makiander.abr@gmail.com>
2019-02-14 17:09:39 -0200

    robot firmware: initialcommit.

---
+++ RobotFirmware/RobotFirmware.ino
@@ -0,0 +1,19 @@
+#define robot_number 1 //Define qual robô esta sendo configurado [0->placa 2] [1->placa 3] [2->placa 6]
+#define TEENSY_DEBUG false
+#include <UnballLibraries.h>
+
+void setup(void) {
+  if(TEENSY_DEBUG){
+    Serial.begin(115200);
+    while( !Serial );
+  }
+
+  Radio::Setup();
+  Motor::Setup();
+  Encoder::Setup();
+  Control::acc = 0;
+}
+
+void loop(){
+  Control::stand();
+}
