505279db008f2773cecb5ff86b8c52a9735eb054
makueba <makiander.abr@gmail.com>
2019-02-18 15:45:05 -0300

    updated extra libraries shell script

--- Libraries/ExtraLibraries/setupExtraLibraries.sh
+++ Libraries/ExtraLibraries/setupExtraLibraries.sh
@@ -1,6 +1,9 @@
 #catch user
 user=$(whoami)
-#remove the library in case it is already installed
+#remove the libraries in case it is already installed
 sudo rm -r /home/$user/Arduino/libraries/RF24
-#installs the library
+sudo rm -r /home/$user/Arduino/libraries/i2c_t3
+
+#installs the libraries
 cp -r RF24 /home/$user/Arduino/libraries/RF24
+cp -r i2c_t3 /home/$user/Arduino/libraries/i2c_t3
