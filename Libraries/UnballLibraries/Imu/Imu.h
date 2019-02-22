5e9045b43c656ae33103a9b985d86a3383d43977
makueba <makiander.abr@gmail.com>
2019-02-21 21:10:57 -0300

    changed wire library

--- Libraries/UnballLibraries/Imu/Imu.h
+++ Libraries/UnballLibraries/Imu/Imu.h
@@ -6,14 +6,15 @@
 #define IMU_NUMBER                  0       //numero gravado no IMU
 #define IMU_I2C_ADDRESS             0x68    // endereçõ de comunicação com IMU
 #define IMU_ACCEL_START             0x3B    // R
-#define IMU_GYRO_START              0x43    // R
 #define IMU_TEMP_START              0x41    // R
+#define IMU_GYRO_START              0x43    // R
 #define PWR_MGMT_1                  0x6B    // R/W
 #define MPU6050_WHO_AM_I            0x75    // R
 #define IMU_ACELL_CONFIG            0x1C    // Configure the accelerometer scale
 #define IMU_GYRO_CONFIG             0x1B    // Configure the gyroscope scale
+#define IMU_FIFO_EN                 0x23
 
-#include <i2c_t3.h>
+#include <Wire.h>
 #include <math.h>
 
 namespace Imu{
@@ -44,44 +45,63 @@ namespace Imu{
         Wire.write(PWR_MGMT_1);     
         Wire.write(0);              //writes 0 to wake the imu
         error = Wire.endTransmission();
-        if(IMU_DEBUG)
-            Serial.print("start: ");Serial.println(error);
+        if(IMU_DEBUG){
+            Serial.print("start: ");
+            Serial.println(error);
+        }
+
+        Wire.beginTransmission(IMU_I2C_ADDRESS);
+        Wire.write(IMU_FIFO_EN);     
+        Wire.write(0xF8);              //writes 0 to wake the imu
+        error = Wire.endTransmission();
+        if(IMU_DEBUG){
+            Serial.print("start: ");
+            Serial.println(error);
+        }
     }
 
     // Configuring accel scale
     // 0 -> +/-2g; 1 -> +/-4g; 2 -> +/-8g; 3 -> +/-16g
     void imuAccelScale(uint8_t scale){
         uint8_t error;
+        scale = scale << 3;
         Wire.beginTransmission(IMU_I2C_ADDRESS);
         Wire.write(IMU_ACELL_CONFIG);
-        Wire.write(scale<<3);
+        Wire.write(scale);
         error = Wire.endTransmission();
-        if(IMU_DEBUG)
-            Serial.print("accel config: ");Serial.println(error);
+        if(IMU_DEBUG){
+            Serial.print("accel config: ");
+            Serial.println(error);
+        }
     }
 
     // Configuring gyro scale
     // 0 -> +/-250; 1 -> +/-500; 2 -> +/-1000; 3 -> +/-2000
     void imuGyroScale(uint8_t scale){
         uint8_t error;
+        scale = scale << 3;
         Wire.beginTransmission(IMU_I2C_ADDRESS);
         Wire.write(IMU_GYRO_CONFIG);
-        Wire.write(scale<<3);
+        Wire.write(scale);
         error = Wire.endTransmission();
-        if(IMU_DEBUG)
-            Serial.print("gyro config: ");Serial.println(error);
+        if(IMU_DEBUG){
+            Serial.print("gyro config: ");
+            Serial.println(error);
+        }
     }
 
     //lê size bytes a partir do endereço address e salva em buffer
     void imuRegRead(uint8_t address, size_t size, int8_t *buffer){
         uint8_t error;
         Wire.beginTransmission(IMU_I2C_ADDRESS);
-        Wire.write(0x41);
+        Wire.write(address);
         error = Wire.endTransmission();
-        if(IMU_DEBUG)
-            Serial.print("imuRegRead: ");Serial.println(error);
+        if(IMU_DEBUG){
+            Serial.print("imuRegRead: ");
+            Serial.println(error);
+        }
 
-        Wire.requestFrom(address, size); 
+        Wire.requestFrom((uint8_t)IMU_I2C_ADDRESS, size); 
         for(uint16_t i = 0; i < size; i++)
             buffer[i] = Wire.read();
     }
@@ -89,7 +109,7 @@ namespace Imu{
     //concatena size bytes, em pares, de from e salva em to
     void to16(int16_t *to, int8_t *from, uint8_t size){
         for(uint8_t i = 0; i < size; i++)
-            to[i] = from[i*2]<<8 | from[(i*2)+1];
+            to[i] = ((int16_t)from[i*2]<<8) | from[(i*2)+1];
     }
 
     //faz a leitura do acelerômetro e calcula o valor em m/s usando accelScale[scale]
@@ -98,9 +118,9 @@ namespace Imu{
         int16_t rawBuffer[3];
         imuRegRead(IMU_ACCEL_START, 6, regBuffer);
         to16(rawBuffer, regBuffer, 3);
-        accel.x = (rawBuffer[0]*9.81)/accelScale[scale];
-        accel.y = (rawBuffer[1]*9.81)/accelScale[scale];
-        accel.z = (rawBuffer[2]*9.81)/accelScale[scale];
+        accel.x = (rawBuffer[0]*9.81)/accelScale[1];
+        accel.y = (rawBuffer[1]*9.81)/accelScale[1];
+        accel.z = (rawBuffer[2]*9.81)/accelScale[1];
     }
 
     //faz a leitura do giroscópio e calcula o valor em graus/s usando gyroScale[scale]
@@ -151,7 +171,7 @@ namespace Imu{
         //    Acceleration at 2g
         //    Clock source at internal 8MHz
         //    The device is in sleep mode.
-        
+
         imuStart();
         imuAccelScale(0);
         imuGyroScale(3);
