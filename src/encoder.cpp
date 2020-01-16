8691f301f41bbd0ee8beb9197e890474d4fe9b4f
Luiz <labm1997@gmail.com>
2020-01-16 12:00:54 -0300

    Saida do encoder agora em ticks/ms

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -4,17 +4,17 @@
 
 namespace Encoder {
 
-    volatile double contadorA = 0;
-    volatile double contadorB = 0;
+    volatile uint64_t contadorA = 0;
+    volatile uint64_t contadorB = 0;
     double timeAnt = 0;
     double presentVelA;
     double presentVelB;
     
     void somaA(){
-        contadorA += 0.0122718463;
+        contadorA++;
     }
     void somaB(){
-        contadorB += 0.0122718463;
+        contadorB++;
     }
 
     void setup() {
@@ -40,8 +40,8 @@ namespace Encoder {
         //desabilita as interrupções para não causar conflito na hr de usar as variaveis
         detachInterrupt(CHANNEL_A_PIN);
         detachInterrupt(CHANNEL_B_PIN);
-        presentVelA = (float) (contadorA/t)*Motor::getMotorDirection(0);
-        presentVelB = (float) (contadorB/t)*Motor::getMotorDirection(1);
+        presentVelA = (double) ((1.0*contadorA)/t)*Motor::getMotorDirection(0);
+        presentVelB = (double) ((1.0*contadorB)/t)*Motor::getMotorDirection(1);
         //habilita as interupções novamente
         attachInterrupt(CHANNEL_A_PIN, somaA, RISING);
         attachInterrupt(CHANNEL_B_PIN, somaB, RISING);
