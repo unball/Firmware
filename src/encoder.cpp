2b9347e4b0ba5f5522faf8342c575c392a3d9697
Luiz <labm1997@gmail.com>
2020-01-21 12:24:53 -0300

    Experimentos com os encoders

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -1,6 +1,7 @@
 #include "encoder.hpp"
 
 #define alpha 1
+#define beta 0.05
 
 namespace Encoder {
 
@@ -9,12 +10,18 @@ namespace Encoder {
     double timeAnt = 0;
     double presentVelA;
     double presentVelB;
+    volatile uint32_t timeVarA = 0, lastTimeA = 0;
+    volatile uint32_t timeVarB = 0, lastTimeB = 0;
     
     void somaA(){
         contadorA++;
+        // timeVarA = beta*(micros()-lastTimeA) + (1-beta)*timeVarA;
+        // lastTimeA = micros();
     }
     void somaB(){
         contadorB++;
+        // timeVarB = micros()-lastTimeB;
+        // lastTimeB = micros();
     }
 
     void setup() {
@@ -31,6 +38,28 @@ namespace Encoder {
         double deltaT = micros()/1000.0 - timeAnt;
         return deltaT;
     }
+
+    // vel encoder(){
+    //     static float prevVA, prevVB;
+    //     double t = timeCounter();
+    //     vel enc;
+    //     detachInterrupt(CHANNEL_A_PIN);
+    //     detachInterrupt(CHANNEL_B_PIN);
+    //     presentVelA = (double) ((timeVarA == 0) ? 0 : 1000.0/(timeVarA));
+    //     presentVelB = (double) ((1.0*contadorA)/t)*Motor::getMotorDirection(1);
+    //     attachInterrupt(CHANNEL_A_PIN, somaA, RISING);
+    //     attachInterrupt(CHANNEL_B_PIN, somaB, RISING);
+
+    //     enc.motorA = alpha* presentVelA + (1-alpha) * prevVA;
+    //     enc.motorB = alpha* presentVelB + (1-alpha) * prevVB;
+
+    //     timeAnt = micros()/1000.0;
+    //     contadorA = 0;
+    //     contadorB = 0;
+    //     prevVA = enc.motorA;
+    //     prevVB = enc.motorB;
+    //     return enc;
+    // }
 
     vel encoder() {
         static float prevVA, prevVB;
