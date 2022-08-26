978138f9f70cbd671e78ac4c1a6386e521159e69
André <andredornelas23@gmail.com>
2022-08-26 17:09:36 -0300

    Suporte a conversão da leitura da bateria

--- src/battery.cpp
+++ src/battery.cpp
@@ -12,13 +12,21 @@ namespace Battery{
 
     /*
         Lê a entrada analógica A0 e retorna a tensão na bateria
+        3.3V 1023
+        2.96V x
     */
 
     void measure(float *voltage){
-        int readValue;
+        // read the input on analog pin 0:
+        int sensorValue = analogRead(BATT);
+        // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 3.2V):
+        float voltage_read = sensorValue * (2.96 / 917.6);
+        *voltage = voltage_read * (7.4 / 2.96);
 
-        readValue = analogRead(BATT);
-        *voltage = map_float(readValue, 0, 1023, 0, 100);
+        Serial.print("A0 CONTÉM[");Serial.print(sensorValue);Serial.println("]");
+        Serial.print("voltagem:[");Serial.print(voltage_read);Serial.println("]V");
+        Serial.println("#####");
+        //*voltage = map_float(readValue, 0, 1023, 0, 100);
     }
 
 }
