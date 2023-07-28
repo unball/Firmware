d063fec8ebe303f3bd8ae07fb2c5d8539d288614
André <andredornelas23@gmail.com>
2023-07-27 22:07:22 -0300

    testa o recebimento wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,56 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
 
-#define WEMOS_DEBUG false
-#define ROBOT_NUMBER 0
+#define WEMOS_DEBUG true
+#define ROBOT_NUMBER 1
 
 #include "radio.hpp"
 #include "motor.hpp"
 #include "waves.hpp"
-#include "battery.hpp"
 
+typedef struct dataStruct
+{
+	int16_t vl;
+	int16_t vr;
+} dataStruct;
 
-Radio::dataStruct vel;
+//Cria uma struct_message chamada myData
+dataStruct vel;
+
+//Funcao de Callback executada quando a mensagem for recebida
+void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len)
+{
+  memcpy(&vel, incomingData, sizeof(vel));
+  Serial.println();
+  Serial.print("Bytes recebidos: ");
+  Serial.println(len);
+  Serial.print("String: (");
+  Serial.print(vel.vl);
+  Serial.println(")");
+  Serial.print("String: (");
+  Serial.print(vel.vr);
+  Serial.println(")");
+}
 
 void setup() {	
 	#if WEMOS_DEBUG
-		Serial.begin(9600);
+  		Serial.begin(115200);
 		while(!Serial);
 		delay(1000);
 		Serial.println("START");
 	#endif
+
+	WiFi.mode(WIFI_STA);
+	if (esp_now_init() != 0) {
+		Serial.println("Erro ao inicializar o ESP-NOW");
+		return;
+	}
+	esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  	esp_now_register_recv_cb(OnDataRecv);
+	
 	Radio::setup(ROBOT_NUMBER, 3);
 	Motor::setup();
-	pinMode(BATT,INPUT);
 	
 }
 
@@ -30,21 +60,21 @@ void loop() {
 		
 		//=========Radio===============
 		// Velocidades a serem lidas do rádio, são estáticas de modo que se Radio::receiveData não receber nada, mantém-se a velocidade anterior
-        static double vl;
-        static double vr;
-		Radio::receiveData(&vl, &vr);
-		Serial.println("###################");
-		Serial.println("Radio:");
-		Serial.print("vl: ");Serial.print(vl);Serial.print("\tvr: ");Serial.println(vr);
-		Serial.println("###################");
+        // static double vl;
+        // static double vr;
+		// Radio::receiveData(&vl, &vr);
+		// Serial.println("###################");
+		// Serial.println("Radio:");
+		// Serial.print("vl: ");Serial.print(vl);Serial.print("\tvr: ");Serial.println(vr);
+		// Serial.println("###################");
 		//=========End Radio===========
 
-		//=========Bateria===============
-		static double voltage;
-		Battery::measure(&voltage);
-		Serial.println("Bateria:");
-		Serial.print("Tensão aproximada: ");Serial.println(voltage);
-		//=========End Bateria===========
+		//=========Wifi===============
+		Serial.println("###################");
+		Serial.println("Radio:");
+		Serial.print("vl: ");Serial.print(vel.vl);Serial.print("\tvr: ");Serial.println(vel.vr);
+		Serial.println("###################");
+		//=========End Wifi===========
 
 		//=========Motor===============
 		Motor::move(0, 100);
