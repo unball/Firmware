9aa242c742ed6488f7a0b0b41456a73435f832ae
André <andredornelas23@gmail.com>
2023-09-21 22:11:03 -0300

    inclui assinatura de métodos

---
+++ include/IMU.hpp
@@ -0,0 +1,17 @@
+#ifndef IMU_HPP
+#define IMU_HP
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_MPU6050.h>
+#include "pins.h"
+
+namespace Imu{
+
+    extern Adafruit_MPU6050 mpu;
+
+    void setup();
+    float get_theta_speed();
+}
+
+#endif
