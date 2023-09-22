467efecacf66ef54d1c91cecdd06f499ee600736
André <andredornelas23@gmail.com>
2023-09-22 11:01:18 -0300

    Refatorando comunicação

---
+++ include/wifi.hpp
@@ -0,0 +1,24 @@
+#ifndef WIFI_H
+#define WIFI_H
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+#include "pins.h"
+
+namespace Wifi{
+
+    typedef struct dataStruct
+    {
+        uint8_t id;
+        double v;
+        double w;
+    } dataStruct;
+
+    void setup(uint8_t robot);
+    void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len);
+    void receiveData(double *v, double *w);
+    bool isCommunicationLost();
+
+}
+
+#endif
---
+++ src/wifi.cpp
@@ -0,0 +1,57 @@
+#include "wifi.hpp"
+
+//TODO: Restar ESP 
+
+namespace Wifi{
+
+    dataStruct temp_vel;
+
+    dataStruct vel;
+
+    volatile static uint32_t lastReceived;
+
+    uint8_t robotNumber;
+
+    double v = 0;
+    double w = 0;
+
+    void setup(uint8_t robot){
+        robotNumber = robot;
+
+        WiFi.mode(WIFI_STA);
+        if (esp_now_init() != 0) {
+            Serial.println("Erro ao inicializar o ESP-NOW");
+            return;
+        }
+
+        esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+        esp_now_register_recv_cb(OnDataRecv);
+    }
+
+    // Callback function, execute when message is received via Wi-Fi
+    void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len)
+    {
+        memcpy(&temp_vel, incomingData, sizeof(vel));
+	    lastReceived = micros();
+    }
+
+    void receiveData(double *v, double *w){
+        // Protecting original data
+        vel = temp_vel;
+        if(vel.id == robotNumber){
+            *v = vel.v;
+            *w = vel.w;
+        }
+    }
+
+    bool isCommunicationLost(){
+        if((micros() - lastReceived) > RADIO_THRESHOLD){
+			// Communication probably failed
+			// if((micros() - lastReceived) > RADIO_RESET_THRESHOLD)
+			// 	ESP.restart();
+            return true;
+		}
+        return false;
+    }
+
+}
