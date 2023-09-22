43957c92af8885ce6da9eed6858ba9bb8a4c1270
André <andredornelas23@gmail.com>
2023-09-22 11:08:49 -0300

    muda os pinos para coincidir com master

--- include/pins.h
+++ include/pins.h
@@ -7,19 +7,19 @@
 
 //TODO: change to typedef file with all macros and structs
 
-//**Motor pins**
-#define PWMA_PIN D8
-#define AIN1_PIN D6
-#define AIN2_PIN D7
+#define PWMA_PIN D4
+#define AIN1_PIN D0
+#define AIN2_PIN D8
 
-#define PWMB_PIN D3
-#define BIN1_PIN D0
-#define BIN2_PIN D4
+#define PWMB_PIN D1
+#define BIN1_PIN D3
+#define BIN2_PIN D2
 
-#define STDY_PIN D5
-
-//**IMU pins**
-#define SCL_PIN D1
-#define SDA_PIN D2
+//**Radio pins**
+#define CE_PIN 0    //RX
+#define CS_PIN RX  //TX
+#define MOSI_PIN D7
+#define MISO_PIN D6
+#define SCK_PIN D5
 
 #endif
