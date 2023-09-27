a44cb7ed27122bd1d534ed101de528d1c070a68e
André <andredornelas23@gmail.com>
2023-09-26 22:17:13 -0300

    diminui threshold da comunicação pra 0.5s

--- include/pins.h
+++ include/pins.h
@@ -2,7 +2,7 @@
 #ifndef PINS_HPP
 #define PINS_HPP
 
-#define RADIO_THRESHOLD 2000000
+#define RADIO_THRESHOLD 500000
 #define RADIO_RESET_THRESHOLD 4000000
 
 //TODO: change to typedef file with all macros and structs
