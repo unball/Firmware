b1b4fe417e21709d6f97e2a1aafbf1acf365d731
André <andredornelas23@gmail.com>
2023-09-27 19:23:59 -0300

    feat: cria arquivo de configurações

---
+++ include/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+/* Main configs*/
+#define WEMOS_DEBUG false
+#define ROBOT_NUMBER 1
+
+/* Wi-Fi */
+const float MAX_POWER = 10.5;
+
+/* Control */
+const float kp = 0.159521;
+const float ki = 0.016864;
+const float kd = 0.016686;
+const float r =	0.016;
+const float L = 0.075;
+
+
+#endif
