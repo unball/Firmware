9ed9fe3ae00d4d740d1c3f3ef00349b29c14ae39
GustavoRosattoSilva <50531178+GustavoRosattoSilva@users.noreply.github.com>
2024-02-23 00:38:49 -0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,21 @@
 #include <motor.hpp>
 #include <waves.hpp>
 #include <wifi.hpp>
+#include "../../include/config.h"
 
-void setup() {	
+void setup() {
+	Wifi::setup(ROBOT_NUMBER);
+	Serial.begin(115200);	
 	#if WEMOS_DEBUG
-  		Serial.begin(115200);
+  		
 		while(!Serial);
 		delay(1000);
 		Serial.println("START");
-	#endif
-	
-	#if WEMOS_DEBUG
 		IMU::setup_debug();
-		Wifi::setup_debug(ROBOT_NUMBER);
+		//Wifi::setup_debug(ROBOT_NUMBER);
 	#else
 		// IMU::setup();
-		Wifi::setup(ROBOT_NUMBER);
+		
 	#endif
 
 	Motor::setup();
@@ -25,9 +25,13 @@ void setup() {
 }
 
 void loop() {
+	Serial.println("LOOP!");
 	#if WEMOS_DEBUG
-		static int16_t vl; 
-		static int16_t vr; 
+		float kp;
+		float ki; 
+		float kd; 
+		float w; 
+		float v;  
 		Serial.println("LOOP!");
 
 		//=========IMU===============
@@ -40,26 +44,45 @@ void loop() {
 		//=========Wifi===============
 		Serial.println("###################");
 		Serial.println("Wi-Fi:");
-        Wifi::receiveData(&vl, &vr);
-		Serial.print("vl: ");Serial.print(vl);Serial.print("\tvr: ");Serial.println(vr);
+        Wifi::receiveData(&kp, &ki, &kd,  &w, &v);
+		Serial.print("kp: ");Serial.print(kp);
+		Serial.print("\tki: ");Serial.println(ki);
+		Serial.print("\tkd: ");Serial.println(kd);
+		Serial.print("\tw: ");Serial.println(w);
+		Serial.print("\tv: ");Serial.println(v);
 		Serial.println("###################");
 		//=========End Wifi===========
 
 		//=========Motor===============
-		Motor::move(0, 100);
-		Motor::move(1, 100);
+		Motor::move(0, 00);
+		Motor::move(1, 00);
 		//=========End Motor===========
 		delay(500);
 	#else
 		static int32_t previous_t;
 		static int32_t t;
 		t = micros();
-
+		#if PID_TUNNER
 		// Loop de controle deve ser executado em intervalos comportados
+		if(t-previous_t >= controlLoopInterval){
+			Serial.println("c0ntr0 interval false 0k!");
+			previous_t = t;
+			Control::stand(false);
+			//Control::actuateNoControl();
+
+		}
+		else{
+			Serial.println("c0ntr0 interval true 0k!");
+			Control::stand(true);
+		}
+		#else
 		if(t-previous_t >= controlLoopInterval){
 			previous_t = t;
 			Control::actuateNoControl();
+
 		}
+		#endif
+	
 
 	#endif
 
