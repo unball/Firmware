65dc4e138edf856db4dd0493d138e38048a2576d
GustavoRosattoSilva <50531178+GustavoRosattoSilva@users.noreply.github.com>
2024-02-24 12:28:18 -0300

    Add files via upload

--- lib/wifi/wifi.cpp
+++ lib/wifi/wifi.cpp
@@ -1,15 +1,9 @@
 #include "wifi.hpp"
 
-
-//TODO: Resetar ESP 
-
-#define MAX_POWER 10.0  //TODO: Test values
-#define WIFI_CHANNEL 12  //TODO: Test values
-
-/* Broadcast address, sends to everyone */
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
 namespace Wifi{
+
+    uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
 
     rcv_message temp_msg;
 
@@ -19,31 +13,30 @@ namespace Wifi{
 
     uint8_t robotNumber;
 
-    void setup_debug(uint8_t robot){
-        robotNumber = robot;
-
-        WiFi.mode(WIFI_STA);
-        if (esp_now_init() != 0) {
-            Serial.println("Erro ao inicializar o ESP-NOW");
-            return;
-        }
-
-        esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
-        esp_now_register_recv_cb(OnDataRecv);
-    }
+    bool useControl = false;
+    bool doTwiddle = false;
 
     void setup(uint8_t robot){
         robotNumber = robot;
 
         WiFi.mode(WIFI_STA);
         if (esp_now_init() != 0) {
+            #if WEMOS_DEBUG
+            Serial.println("Erro ao inicializar o ESP-NOW");
+            #endif
             return;
         }
         WiFi.setOutputPower(MAX_POWER);
 
         esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
         esp_now_register_recv_cb(OnDataRecv);
-        lastReceived = micros();
+        while (!useControl && !doTwiddle){
+            Serial.println("No Mode");
+            receiveConfig(&useControl, &doTwiddle);
+        }
+        
+                        
+        
     }
 
     // Callback function, execute when message is received via Wi-Fi
@@ -51,49 +44,43 @@ namespace Wifi{
     {
         memcpy(&temp_msg, incomingData, sizeof(msg));
 	    lastReceived = micros();
-        // TODO: last Received deveria estar aqui ou em receiveData?
+        // TODO: lastReceived deveria estar aqui ou em receiveData?
     }
 
-void sendWifi(float ew){
-    /* Sends the message using ESP-NOW */
-    esp_now_send(broadcastAddress, (uint8_t *) &ew, sizeof(ew));
-    delay(3);
-  
-}
-
-    #if PID_TUNNER
-
-    /// @brief Receive data copying from temp struct to global struct
-    /// @param kp Constoante proporcional
-    /// @param ki Constante integral
-    /// @param kd Constante derivativa
-    void receiveData(float* kp, float* ki, float* kd, float* w, float* v){
-            // Protecting original data
-            msg = temp_msg;
-            if(msg.id == robotNumber){
-                // Demultiplexing and decoding the velocities
-                *kp = (float)msg.kp/100;
-                *ki = (float)msg.ki/100;
-                *kd = (float)msg.kd/100;
-                *w = (float)msg.w/100;
-                *v = (float)msg.v/100;
-            }
-        }
-
-    #else
-    /// @brief Receive data copying from temp struct to global struct
-    /// @param vl reference to the velocity
-    /// @param vr reference to the velocity
-    void receiveData(int16_t *vl, int16_t *vr){
+    /// @brief Receive system's configurations in wether to use control or to do the PID Tunner routine, by copying from temp struct to global struct
+    /// @param control reference to the control flag
+    /// @param twiddle reference to the PID Tunner flag
+    void receiveConfig(bool *control, bool *twiddle){
         // Protecting original data
         msg = temp_msg;
         if(msg.id == robotNumber){
-            // Demultiplexing and decoding the velocities
-            *vl = msg.vl;
-            *vr = msg.vr;
+            /*control = msg.control;
+            *twiddle = msg.control;*/
+            if (msg.control == 1){
+                *control = true;
+            }
+
+            else if (msg.control == 2){
+                *twiddle = true;
+            }                    
         }
     }
-    #endif
+
+    /// @brief Receive data copying from temp struct to global struct
+    /// @param v reference to the linear velocity
+    /// @param w reference to the angular velocity
+    void receiveData(double *kp, double *ki, double *kd, double *v, double *w){
+        // Protecting original data
+        msg = temp_msg;
+        if(msg.id == robotNumber){
+            // Demultiplexing and decoding the velocities and constants
+            *kp = ((float)msg.kp) / 100;
+            *ki = ((float)msg.ki) / 100;
+            *kd = ((float)msg.kd) / 100;
+            *v  = ((float)msg.v) * 2.0 / 32767;
+            *w  = ((float)msg.w) * 64.0 / 32767;
+        }
+    }
 
     bool isCommunicationLost(){
         if((micros() - lastReceived) > communicationTimeout){
@@ -103,6 +90,14 @@ void sendWifi(float ew){
             return true;
 		}
         return false;
+    }
+
+    void sendResponse(double erro){
+        snd_message robotStatus;
+        robotStatus.id = ROBOT_NUMBER;
+        robotStatus.response  = true;
+        robotStatus.value = (int16_t)(erro * 100);
+        esp_now_send(broadcastAddress, (uint8_t *) &robotStatus, sizeof(snd_message));
     }
 
 }
--- lib/wifi/wifi.hpp
+++ lib/wifi/wifi.hpp
@@ -3,42 +3,37 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include "../../include/config.h"
-
+#
 
 namespace Wifi{
 
-    #if PID_TUNNER
-    struct snd_message
+    struct rcv_message
     {
-        int16_t err;
-    };
-
-     struct rcv_message
-    {
-        int16_t id;
+        uint8_t control;
+        uint8_t id;
         int16_t kp;
         int16_t ki;
         int16_t kd;
-        int16_t w;
         int16_t v;
+        int16_t w;
     };
-    void receiveData(float* kp, float* ki, float* kd, float* v, float* w);
-    void sendWifi(float erro);
-    
 
-    #else
-    struct rcv_message{
-        int16_t id;
-        int16_t vl;
-        int16_t vr;
+    struct snd_message{
+        uint8_t id;
+        bool response;
+        int16_t value;
     };
-    void receiveData(int16_t *vl, int16_t *vr);
 
-    #endif
-    void setup_debug(uint8_t robot);
+    extern bool useControl;
+    extern bool doTwiddle;
+
     void setup(uint8_t robot);
-    void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len);
+    void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len);
+    void receiveConfig(bool *control, bool *twiddle);
+    void receiveData(double *kd, double *ki, double *kp, double *v, double *w);
     bool isCommunicationLost();
+    void sendResponse(double erro);
 
 }
+
 #endif
