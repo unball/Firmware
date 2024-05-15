9003a72cb73bac086d88462d778396f0fe4b2746
MariaClaudia1328 <mariaclaudia8.0@hotmail.com>
2024-05-15 13:52:29 +0000

    calcula vl e vr

--- lib/motor/motor.cpp
+++ lib/motor/motor.cpp
@@ -24,6 +24,38 @@ namespace Motor {
         pinMode(BIN2_PIN, OUTPUT);
     }
 
+    int16_t deadzone(int16_t vin, int16_t up, int16_t down){
+        if(vin != 0){
+            return vin+up ? vin > 0 : vin - abs(down);
+        }
+        return 0;
+    }
+
+    int16_t speed2motors(int16_t v, int16_t w){
+
+        int16_t vr = 0;
+        int16_t vl = 0;
+        float L = 0.075;
+        float r = 0.016;
+
+        // Computa a velocidade angular de rotação de cada roda
+        vr = (v + (L/2)*w) / r;
+        vl = (v - (L/2)*w) / r;
+
+        // Computa a deadzone do motor.
+        int16_t vr_sat_min = vr ? vr < 255 : 255;
+        int16_t vl_sat_min = vl ? vl < 255: 255;
+
+        int16_t vr_sat_max = vr_sat_min ? vr_sat_min > -255 : -255;
+        int16_t vl_sat_max = vl_sat_min ? vl_sat_min > -255 : -255;
+
+        vr = deadzone(vr_sat_max, 32, -32);
+        vl = deadzone(vl_sat_max, 32, -32);
+
+        return vr, vl;
+
+    }
+
     void move(uint8_t motor, int32_t power) {
         //Allows current for the motors
         digitalWrite(STBY_PIN, HIGH);
