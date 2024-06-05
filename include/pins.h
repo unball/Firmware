c68fab282696bb0fbc1e8ab253e7119f186ccb85
GustavoRosattoSilva <gustavorosattosilva@yahoo.com>
2024-06-05 14:48:04 -0300

    Atualiza pinos

--- include/pins.h
+++ include/pins.h
@@ -1,21 +1,22 @@
+
 #ifndef PINS_HPP
 #define PINS_HPP
 
 //TODO: change to typedef file with all macros and structs
 
 //**Motor pins**
-#define PWMA_PIN D8
-#define AIN1_PIN D6
-#define AIN2_PIN D7
+#define PWMA_PIN 12
+#define AIN1_PIN 9
+#define AIN2_PIN 11
 
-#define PWMB_PIN D3
-#define BIN1_PIN D0
-#define BIN2_PIN D4
+#define PWMB_PIN 18
+#define BIN1_PIN 05
+#define BIN2_PIN 16
 
-#define STBY_PIN D5
+#define STBY_PIN 7
 
 //**IMU pins**
-#define SCL_PIN D1
-#define SDA_PIN D2
+#define SCL_PIN 35
+#define SDA_PIN 33
 
 #endif
