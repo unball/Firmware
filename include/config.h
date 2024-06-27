cfb510cd76d87ddc45582eb3201d9d8e49da1045
maranhas02 <221029051@aluno.unb.br>
2024-06-27 10:07:08 -0300

    ⚙️ Deadzone para 32

--- include/config.h
+++ include/config.h
@@ -7,7 +7,7 @@
 #define TWIDDLE false
 #define ROBOT_NUMBER 0
 
-/* Wi-Fi */
+/* Wi-Fi */ 
 const int communicationTimeout = 500000;
 const int resetTimeout = 4000000;
 const float MAX_POWER = 10.0;
@@ -18,6 +18,6 @@ const int controlLoopInterval= 200;   // actuation interval in ms
 /* Robot parameters */
 const float r =	0.01525;
 const float L = 0.0605;
-const int motor_deadzone = 65;
+const int motor_deadzone = 32;
 
 #endif
