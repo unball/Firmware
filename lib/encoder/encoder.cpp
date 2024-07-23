eef02dec14e4ed165c751af7a4db1f0c504ace26
André <andredornelas23@gmail.com>
2024-07-23 11:41:11 -0300

    :recycle: removes previous encoder reading

--- lib/encoder/encoder.cpp
+++ lib/encoder/encoder.cpp
@@ -7,12 +7,10 @@ namespace Encoder {
 
     volatile uint64_t counter_A = 0;
     volatile uint64_t counter_B = 0;
-    double prev_time = 0;
+    uint32_t prev_time = 0;
     double present_vel;
-    double presentVelA;
-    double presentVelB;
-    volatile uint32_t timeVarA = 0, lastTimeA = 0;
-    volatile uint32_t timeVarB = 0, lastTimeB = 0;
+    volatile uint64_t counter = 0;
+    double present_speed;
     
     void add_A(){
         counter_A++;
@@ -22,8 +20,19 @@ namespace Encoder {
     }
 
     void setup() {
-        attachInterrupt(CHANNEL_A_PIN, add_A, RISING);
-        attachInterrupt(CHANNEL_B_PIN, add_B, RISING);
+        attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN), add_A, CHANGE);
+        attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN), add_B, CHANGE);
+        // attachInterrupt(CHANNEL_A_PIN, calc, CHANGE);
+        // attachInterrupt(CHANNEL_B_PIN, calc, CHANGE);
+    }
+
+    void calc() {
+        uint32_t static curr_time, prev_time; // tem que ver como iniciar
+
+        curr_time = micros();
+        uint32_t T_us = curr_time - prev_time; // importante estar em uint32, pois a diferença é sempre igual ao tempo
+        present_speed = (2*PI)/(2*48*(T_us*1e-6));
+        prev_time = curr_time;
     }
 
     void resetEncoders() {
@@ -31,56 +40,51 @@ namespace Encoder {
         counter_B = 0;
     }
 
-    double time_counter(){
-        double deltaT = micros()/1000.0 - prev_time;
+    uint32_t time_counter() {
+        uint32_t deltaT = micros() - prev_time;
         return deltaT;
     }
 
-    vel encoder() {
-        static float prevVA, prevVB;
 
-        double t = time_counter();
-        vel enc;
+    double get_ticks_per_us() {
+        uint32_t t = time_counter();
 
-        detachInterrupt(CHANNEL_A_PIN);
-        detachInterrupt(CHANNEL_B_PIN);
-
-        presentVelA = (double) ((1.0*counter_A)/t)*Motor::getMotorDirection(0);
-        presentVelB = (double) ((1.0*counter_B)/t)*Motor::getMotorDirection(1);
-
-        attachInterrupt(CHANNEL_A_PIN, add_A, RISING);
-        attachInterrupt(CHANNEL_B_PIN, add_B, RISING);
-
-        enc.motorA = alpha* presentVelA + (1-alpha) * prevVA;
-        enc.motorB = alpha* presentVelB + (1-alpha) * prevVB;
-        
-        prev_time = micros()/1000.0;
-        counter_A = 0;
-        counter_B = 0;
-        prevVA = presentVelA;
-        prevVB = presentVelB;
-        return enc;
-    }
-
-    double get_ticks_per_ms() {
-        static double prev_vel;
-        double encoder_vel;
-        double t = time_counter();
-
-        detachInterrupt(CHANNEL_A_PIN);
-        detachInterrupt(CHANNEL_B_PIN);
+        detachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN));
+        detachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN));
 
         present_vel = (double) ((1.0*(counter_A+counter_B))/t);
 
-        attachInterrupt(CHANNEL_A_PIN, add_A, CHANGE);
-        attachInterrupt(CHANNEL_B_PIN, add_B, CHANGE);
-
-        encoder_vel = alpha * present_vel + (1-alpha) * prev_vel;
+        attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN), add_A, CHANGE);
+        attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN), add_B, CHANGE);
         
-        prev_time = micros()/1000.0;
-        counter_A = 0;
-        counter_B = 0;
-        prev_vel = present_vel;
+        prev_time = micros();
+        resetEncoders();
         return present_vel;
     }
 }
+
+    // vel encoder() {
+    //     static float prevVA, prevVB;
+
+    //     double t = time_counter();
+    //     vel enc;
+
+    //     detachInterrupt(CHANNEL_A_PIN);
+    //     detachInterrupt(CHANNEL_B_PIN);
+
+    //     presentVelA = (double) ((1.0*counter_A)/t)*Motor::getMotorDirection(0);
+    //     presentVelB = (double) ((1.0*counter_B)/t)*Motor::getMotorDirection(1);
+
+    //     attachInterrupt(CHANNEL_A_PIN, add_A, RISING);
+    //     attachInterrupt(CHANNEL_B_PIN, add_B, RISING);
+
+    //     enc.motorA = alpha* presentVelA + (1-alpha) * prevVA;
+    //     enc.motorB = alpha* presentVelB + (1-alpha) * prevVB;
+        
+    //     prev_time = micros()/1000.0;
+    //     counter_A = 0;
+    //     counter_B = 0;
+    //     prevVA = presentVelA;
+    //     prevVB = presentVelB;
+    //     return enc;
+    // }
