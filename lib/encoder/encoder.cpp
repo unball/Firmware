5f132d09994b42d48c6ded1fde67548a128f5b17
André <andredornelas23@gmail.com>
2024-07-28 11:34:39 -0300

    :gear: Calcula a velocidade a partir da temporização entre ondas do encoder

--- lib/encoder/encoder.cpp
+++ lib/encoder/encoder.cpp
@@ -1,90 +1,56 @@
 #include "encoder.hpp"
 
-#define alpha 1
+#define alpha 0.95
 #define beta 0.05
 
 namespace Encoder {
 
-    volatile uint64_t counter_A = 0;
-    volatile uint64_t counter_B = 0;
-    uint32_t prev_time = 0;
-    double present_vel;
-    volatile uint64_t counter = 0;
+    // hw_timer_t *Timer0_Cfg = NULL;
+    // bool Measurement_InProgress = false;
+    // uint64_t Measured_Time = 0;
+    // uint64_t Measured_Freq = 0;
+
     double present_speed;
-    
-    void add_A(){
-        counter_A++;
-    }
-    void add_B(){
-        counter_B++;
-    }
+
+    // void IRAM_ATTR Ext_INT1_ISR()  {
+    //     if(Measurement_InProgress == false)  {
+    //         Measurement_InProgress = true;
+    //         timerWrite(Timer0_Cfg, 0);
+    //         timerStart(Timer0_Cfg);
+    //     }
+    //     else{
+    //         Measured_Time = timerRead(Timer0_Cfg);
+    //         timerStop(Timer0_Cfg);
+    //         Measurement_InProgress = false;
+    //         if(Measured_Time == 0) {
+    //             Measured_Freq = 0;
+    //         }
+    //         else  {
+    //             Measured_Freq = (40000000/Measured_Time);
+    //         }
+    //     }
+    // }
 
     void setup() {
-        attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN), add_A, CHANGE);
-        attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN), add_B, CHANGE);
-        // attachInterrupt(CHANNEL_A_PIN, calc, CHANGE);
-        // attachInterrupt(CHANNEL_B_PIN, calc, CHANGE);
+        pinMode(CHANNEL_A_PIN, INPUT);
+        pinMode(CHANNEL_B_PIN, INPUT);
+        attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN), calc, RISING);
+        // attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN), calc, RISING);
+        // // Configure Timer0
+        // Timer0_Cfg = timerBegin(0, 2, true);
     }
 
-    void calc() {
+    void IRAM_ATTR calc() {
         uint32_t static curr_time, prev_time; // tem que ver como iniciar
 
         curr_time = micros();
-        uint32_t T_us = curr_time - prev_time; // importante estar em uint32, pois a diferença é sempre igual ao tempo
-        present_speed = (2*PI)/(2*48*(T_us*1e-6));
+        uint32_t T_us = curr_time - prev_time;
+        present_speed = (2*PI)/(12*(T_us*1e-6));
+
         prev_time = curr_time;
     }
 
-    void resetEncoders() {
-        counter_A = 0;
-        counter_B = 0;
-    }
-
-    uint32_t time_counter() {
-        uint32_t deltaT = micros() - prev_time;
-        return deltaT;
-    }
-
-
-    double get_ticks_per_us() {
-        uint32_t t = time_counter();
-
-        detachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN));
-        detachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN));
-
-        present_vel = (double) ((1.0*(counter_A+counter_B))/t);
-
-        attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN), add_A, CHANGE);
-        attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN), add_B, CHANGE);
-        
-        prev_time = micros();
-        resetEncoders();
-        return present_vel;
+    double get_w() {
+        return present_speed;
     }
 }
-
-    // vel encoder() {
-    //     static float prevVA, prevVB;
-
-    //     double t = time_counter();
-    //     vel enc;
-
-    //     detachInterrupt(CHANNEL_A_PIN);
-    //     detachInterrupt(CHANNEL_B_PIN);
-
-    //     presentVelA = (double) ((1.0*counter_A)/t)*Motor::getMotorDirection(0);
-    //     presentVelB = (double) ((1.0*counter_B)/t)*Motor::getMotorDirection(1);
-
-    //     attachInterrupt(CHANNEL_A_PIN, add_A, RISING);
-    //     attachInterrupt(CHANNEL_B_PIN, add_B, RISING);
-
-    //     enc.motorA = alpha* presentVelA + (1-alpha) * prevVA;
-    //     enc.motorB = alpha* presentVelB + (1-alpha) * prevVB;
-        
-    //     prev_time = micros()/1000.0;
-    //     counter_A = 0;
-    //     counter_B = 0;
-    //     prevVA = presentVelA;
-    //     prevVB = presentVelB;
-    //     return enc;
-    // }
--- lib/encoder/encoder.hpp
+++ lib/encoder/encoder.hpp
@@ -10,15 +10,8 @@ namespace Encoder{
     }vel;
     
     void setup();
-    void add_A();
-    void add_B();
-    void calc();
-    void calc2();
-    void resetEncoders();
-    uint32_t time_counter();
-    // vel encoder();
-    double get_ticks_per_us();
-    // double get_w();
+    void IRAM_ATTR calc();
+    double get_w();
 
 }
 #endif
