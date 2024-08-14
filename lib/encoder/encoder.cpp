093844e1d2796a79e658cd0c2bba3e2d9215cf3c
André <andredornelas23@gmail.com>
2024-08-14 10:48:49 -0300

    :rocket: Implementa leitura do encoder e cálculo da velocidade usando Timers próprios da biblioteca do ESP

--- lib/encoder/encoder.cpp
+++ lib/encoder/encoder.cpp
@@ -7,57 +7,99 @@ const int watchdog_timer = 500000; // us
 
 namespace Encoder {
 
-    // hw_timer_t *Timer0_Cfg = NULL;
-    // bool Measurement_InProgress = false;
-    // uint64_t Measured_Time = 0;
-    // uint64_t Measured_Freq = 0;
+    hw_timer_t *Timer0_Cfg = NULL;
 
-    double present_speed;
-    uint32_t prev_time = 0;
+    volatile double present_speed;
 
-    // void IRAM_ATTR Ext_INT1_ISR()  {
-    //     if(Measurement_InProgress == false)  {
-    //         Measurement_InProgress = true;
-    //         timerWrite(Timer0_Cfg, 0);
-    //         timerStart(Timer0_Cfg);
-    //     }
-    //     else{
-    //         Measured_Time = timerRead(Timer0_Cfg);
-    //         timerStop(Timer0_Cfg);
-    //         Measurement_InProgress = false;
-    //         if(Measured_Time == 0) {
-    //             Measured_Freq = 0;
-    //         }
-    //         else  {
-    //             Measured_Freq = (40000000/Measured_Time);
-    //         }
-    //     }
-    // }
+    void IRAM_ATTR Ext_INT1_ISR() {
+        uint64_t T_us = timerReadMicros(Timer0_Cfg);
+
+        present_speed = (double) (2*PI)/(12*(T_us*1e-6));
+
+        timerRestart(Timer0_Cfg);
+    }
 
     void setup() {
         pinMode(CHANNEL_A_PIN, INPUT);
-        pinMode(CHANNEL_B_PIN, INPUT);
-        attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN), calc, RISING);
-        // attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN), calc, RISING);
-        // // Configure Timer0
-        // Timer0_Cfg = timerBegin(0, 2, true);
-    }
-
-    void IRAM_ATTR calc() {
-        uint32_t curr_time = micros();
-        uint32_t T_us = curr_time - prev_time;
-
-        present_speed = (2*PI)/(12*(T_us*1e-6));
-
-        prev_time = curr_time;
+        attachInterrupt(CHANNEL_A_PIN, Ext_INT1_ISR, RISING);
+        // Configure Timer0
+        Timer0_Cfg = timerBegin(0, 2, true);
+        timerWrite(Timer0_Cfg, 0);
+        timerStart(Timer0_Cfg);
     }
 
     double get_w() {
-
-        if ((micros() - prev_time) > watchdog_timer) {
+        if (timerReadMicros(Timer0_Cfg) > watchdog_timer) {
             present_speed = 0;
         }
 
         return present_speed;
     }
 }
+
+
+
+
+// #include "encoder.hpp"
+
+// #define alpha 0.95
+// #define beta 0.05
+
+// const int watchdog_timer = 500000; // us
+
+// namespace Encoder {
+
+//     // hw_timer_t *Timer0_Cfg = NULL;
+//     // bool Measurement_InProgress = false;
+//     // uint64_t Measured_Time = 0;
+//     // uint64_t Measured_Freq = 0;
+
+//     double present_speed;
+//     uint32_t prev_time = 0;
+
+//     // void IRAM_ATTR Ext_INT1_ISR()  {
+//     //     if(Measurement_InProgress == false)  {
+//     //         Measurement_InProgress = true;
+//     //         timerWrite(Timer0_Cfg, 0);
+//     //         timerStart(Timer0_Cfg);
+//     //     }
+//     //     else{
+//     //         Measured_Time = timerRead(Timer0_Cfg);
+//     //         timerStop(Timer0_Cfg);
+//     //         Measurement_InProgress = false;
+//     //         if(Measured_Time == 0) {
+//     //             Measured_Freq = 0;
+//     //         }
+//     //         else  {
+//     //             Measured_Freq = (40000000/Measured_Time);
+//     //         }
+//     //     }
+//     // }
+
+//     void setup() {
+//         pinMode(CHANNEL_A_PIN, INPUT);
+//         pinMode(CHANNEL_B_PIN, INPUT);
+//         attachInterrupt(digitalPinToInterrupt(CHANNEL_A_PIN), calc, RISING);
+//         // attachInterrupt(digitalPinToInterrupt(CHANNEL_B_PIN), calc, RISING);
+//         // // Configure Timer0
+//         // Timer0_Cfg = timerBegin(0, 2, true);
+//     }
+
+//     void IRAM_ATTR calc() {
+//         uint32_t curr_time = micros();
+//         uint32_t T_us = curr_time - prev_time;
+
+//         present_speed = (2*PI)/(12*(T_us*1e-6));
+
+//         prev_time = curr_time;
+//     }
+
+//     double get_w() {
+
+//         if ((micros() - prev_time) > watchdog_timer) {
+//             present_speed = 0;
+//         }
+
+//         return present_speed;
+//     }
+// }
