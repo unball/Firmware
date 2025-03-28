98f8748753ab8bda658b68bbebf330405832c93f
adornelas <andredornelas23@gmail.com>
2025-03-28 17:46:57 -0300

    :shamrock: Improve ESP32 test for automated checking

--- tests_manual/test_esp32/main.cpp
+++ tests_manual/test_esp32/main.cpp
@@ -3,7 +3,13 @@
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("✅ ESP32 is alive and communicating over Serial!");
+
+  Serial.println("[TEST START] ESP32 basic check");
+
+  // If we got this far, the ESP32 is alive
+  Serial.println("ESP32 is running and responded to upload.");
+
+  Serial.println("[TEST RESULT] PASS");
 }
 
 void loop() {
