5287c538ac58247ab38e5bc77cc98474fbe783ea
adornelas <andredornelas23@gmail.com>
2025-04-21 10:30:25 -0300

    test: Adjust motor speed and improve test output for motor and encoder functionality

--- tests_manual/test_motors_and_encoders/main.cpp
+++ tests_manual/test_motors_and_encoders/main.cpp
@@ -5,14 +5,13 @@
 #define MIN_SPEED_RAD_S 1.0
 
 void testMotorWithEncoder(uint8_t motorId, const char* name) {
-  Motor::move(motorId, 200);  // Positive power for forward motion
+  Motor::move(motorId, 100);
   delay(TEST_DURATION_MS);
   Motor::stop();
-
-  double w = Encoder::getMotorSpeeds();
+  double w = (motorId == MOTOR_LEFT ? Encoder::getMotorSpeeds().motorLeft : Encoder::getMotorSpeeds().motorRight);
   Serial.printf("%s speed: %.2f rad/s\n", name, w);
 
-  if (w > MIN_SPEED_RAD_S) {
+  if (abs(w) > MIN_SPEED_RAD_S) {
     Serial.printf("[TEST RESULT] %s: PASS\n", name);
   } else {
     Serial.printf("[TEST RESULT] %s: FAIL\n", name);
@@ -22,18 +21,16 @@ void testMotorWithEncoder(uint8_t motorId, const char* name) {
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("[TEST START] Motor + Encoder (using user libs)");
+  Serial.println("[TEST START] Motor + Encoder");
 
   Motor::setup();
   Encoder::setup();
 
-  delay(500);
-
-  testMotorWithEncoder(MOTOR_LEFT, "Motor A");
-  delay(1000);
-  testMotorWithEncoder(MOTOR_RIGHT, "Motor B");
 }
 
 void loop() {
-  // No loop needed
+  delay(500);
+  testMotorWithEncoder(MOTOR_LEFT, "Motor A (LEFT)");
+  delay(500);
+  testMotorWithEncoder(MOTOR_RIGHT, "Motor B (RIGHT)");
 }
